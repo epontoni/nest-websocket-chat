3ae3950c1e326f8d0b57264c04f389b1969e6785
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2023-12-31 04:56:22 -0300

    Addition of the public folder and installation of @nestjs/serve-static to serve statics

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/chat.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ChatModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ChatModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
 })
 export class AppModule {}
