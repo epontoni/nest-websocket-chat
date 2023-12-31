2ee4e427963ec8547bba0f08a4370f6cb8543ca2
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2023-12-31 04:48:05 -0300

    Socket.io installation and websockets server configuration

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,23 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
+import { OnModuleInit } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnModuleInit {
+  @WebSocketServer()
+  public server: Server;
+
   constructor(private readonly chatService: ChatService) {}
+  onModuleInit() {
+    this.server.on('connection', (socket: Socket) => {
+      // New client connected
+      console.log('socket connected');
+      
+      // Client disconnected
+      socket.on('disconnect', () => {
+        console.log('socket disconnected');
+      });
+    });
+  }
 }
